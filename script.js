fd4c6e54d71493e0420584e820391f18e5be4ed7
RayanneP <rayannepicinidasilva@gmail.com>
2025-06-23 11:03:48 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
-let lista = document.querySelectorAll(".item");
-let next = document.getElementById("proximo");
-let prev = document.getElementById("anterior");
+const swiper = new Swiper('.slider-wrapper', {
+  loop: true,
+  grapCursor: true,
+  spaceBetween: 25,
 
-let contar = lista.length;
-let ativo = 0;
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
 
-next.onclick = () => {
-    let desativar = document.querySelector('.ativo');
-    desativar.classList.remove('ativo');
-    ativo = (ativo >= contar - 1) ? 0 : ativo + 1;
-    lista[ativo].classList.add('ativo');
-}
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 
-prev.onclick = () => {
-    let desativar = document.querySelector('.ativo');
-    desativar.classList.remove('ativo');
-    ativo = (ativo <= 0) ? contar - 1 : ativo - 1;
-    lista[ativo].classList.add('ativo');
-}
+  //reponsividade
+
+  breakpoints: {
+    0: {
+        slidesPerView: 1
+    },
+    768: {
+        slidesPerView: 2
+    },
+    1024: {
+        slidesPerView: 3
+    }
+  }
+});

b0f931ee42fddf47f985b2a265cdc766a19203e1
Dani⭐ <danielli.freitas1907@gmail.com>
2025-06-23 16:15:38 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -30,3 +30,41 @@ const swiper = new Swiper('.slider-wrapper', {
     }
   }
 });
+
+const titleElement = document.querySelector('#name');
+const text = "Receitas";
+let index = 0;
+let isTyping = true;
+let currentColor = document.documentElement.classList.contains('light') ? 'black' : '#fff';
+
+// função para animar o texto (digitnado e apagando)
+function animateText() {
+    if(isTyping) {
+        if (index < text.length) {
+            titleElement.textContent = text.slice(0, index + 1);
+            index ++;
+        } else {
+            isTyping = false;
+        }
+    } else {
+        if (index > 1) {
+            titleElement.textContent = text.slice(0, index - 1);
+            index --;
+        } else {
+            isTyping = true;
+            // alterna a cor entre preto e laranja
+            currentColor = currentColor === (document.documentElement.classList.contains('light') ? 'black' : '#fff') ? '#C94C16' : (document.documentElement.classList.contains('light') ? 'black' : '#fff');
+            titleElement.style.color = currentColor;
+        }
+    }
+    setTimeout(animateText, 300);
+}
+
+function updateTextColor() {
+    currentColor = document.documentElement.classList.contains('light') ? 'black' : '#fff';
+    titleElement.style.color = currentColor;
+}
+
+document.addEventListener('DOMContentLoaded', animateText);
+updateTextColor()
+
